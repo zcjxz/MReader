6b40261f93738af1c27234938e734d4046b90e63
zcjxz <106798607@qq.com>
2019-01-25 16:54:56 +0800

    找回之前的文件

--- app/src/main/java/com/zcj/mreader/ui/WebActivity.java
+++ app/src/main/java/com/zcj/mreader/ui/WebActivity.java
@@ -1,24 +1,104 @@
 package com.zcj.mreader.ui;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.zcj.mreader.R;
+import com.zcj.mreader.base.BaseActivity;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class WebActivity extends AppCompatActivity {
-
+public class WebActivity extends BaseActivity {
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
     @BindView(R.id.webview)
     WebView webView;
+    @BindView(R.id.floatButton)
+    FloatingActionButton floatButton;
+    String currentUrl="";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web);
+        Intent intent = getIntent();
+        currentUrl = intent.getStringExtra("url");
         ButterKnife.bind(this);
-        String url = getIntent().getStringExtra("url");
-        webView.loadUrl(url);
+        //设置toolbar
+        setSupportActionBar(toolbar);
+        //设置返回按钮
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar!=null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+        initWebView();
+        webView.loadUrl(currentUrl);
+
+        floatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent shareIntent=new Intent(Intent.ACTION_SEND);
+                shareIntent.putExtra(Intent.EXTRA_TEXT,currentUrl);
+                shareIntent.setType("text/plain");
+                startActivity(Intent.createChooser(shareIntent,"分享干货"));
+            }
+        });
+    }
+
+    private void initWebView(){
+        //设置webView支持js
+        webView.getSettings().setJavaScriptEnabled(true);
+        //设置webView点击链接时不跳其他浏览器
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                toolbar.setTitle("加载中。。。");
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                currentUrl=url;
+            }
+        });
+        //webView获取加载的url的title
+        WebChromeClient mcc=new WebChromeClient(){
+            @Override
+            public void onReceivedTitle(WebView view, String title) {
+                super.onReceivedTitle(view, title);
+                toolbar.setTitle(title);
+            }
+
+        };
+        webView.setWebChromeClient(mcc);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()){
+            webView.goBack();
+        }else{
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                finish();
+        }
+        return true;
     }
 }
